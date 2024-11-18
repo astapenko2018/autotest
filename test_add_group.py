fe5c9705c9191fb5695742cf2b11926fc021d9d7
Олег <44393834+astapenko2018@users.noreply.github.com>
2024-11-18 21:56:47 +0300

    Выделены вспомогательные методы и классы

--- test_add_group.py
+++ test_add_group.py
@@ -9,30 +9,63 @@ from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from group import Group
+
 
 class TestAddressbook():
-  def setup_method(self, method):
-    self.driver = webdriver.Chrome()
-    self.vars = {}
-  
-  def teardown_method(self, method):
-    self.driver.quit()
-  
-  def test_addressbook(self):
-    self.driver.get("http://localhost/addressbook/")
-    self.driver.find_element(By.NAME, "user").send_keys("admin")
-    self.driver.find_element(By.NAME, "pass").click()
-    self.driver.find_element(By.NAME, "pass").send_keys("secret")
-    self.driver.find_element(By.CSS_SELECTOR, "input:nth-child(7)").click()
-    self.driver.find_element(By.LINK_TEXT, "groups").click()
-    self.driver.find_element(By.NAME, "new").click()
-    self.driver.find_element(By.NAME, "group_name").click()
-    self.driver.find_element(By.NAME, "group_name").send_keys("test1")
-    self.driver.find_element(By.NAME, "group_header").click()
-    self.driver.find_element(By.NAME, "group_header").send_keys("werwegw")
-    self.driver.find_element(By.NAME, "group_footer").click()
-    self.driver.find_element(By.NAME, "group_footer").send_keys("qweqwe")
-    self.driver.find_element(By.NAME, "submit").click()
-    self.driver.find_element(By.LINK_TEXT, "group page").click()
-    self.driver.find_element(By.LINK_TEXT, "Logout").click()
-  
+    def setup_method(self, method):
+        self.wd = webdriver.Chrome()
+        self.vars = {}
+
+    def teardown_method(self, method):
+        self.wd.quit()
+
+    def test_add_add_group(self):
+        self.open_home_page()
+        self.login(username="admin", password="secret")
+        self.open_groups_page()
+        self.create_group(Group(name="test1", header="werwegw", footer="qweqwe"))
+        self.return_to_groups_page()
+        self.logout()
+
+    def test_add_add_empty_group(self):
+        self.open_home_page()
+        self.login(username="admin", password="secret")
+        self.open_groups_page()
+        self.create_group(Group(name="", header="", footer=""))
+        self.return_to_groups_page()
+        self.logout()
+
+    def logout(self):
+        # logout
+        self.wd.find_element(By.LINK_TEXT, "Logout").click()
+
+    def return_to_groups_page(self):
+        # return to groups page
+        self.wd.find_element(By.LINK_TEXT, "group page").click()
+
+    def create_group(self, group):
+        # init group creation
+        self.wd.find_element(By.NAME, "new").click()
+        self.wd.find_element(By.NAME, "group_name").click()
+        self.wd.find_element(By.NAME, "group_name").send_keys(group.name)
+        self.wd.find_element(By.NAME, "group_header").click()
+        self.wd.find_element(By.NAME, "group_header").send_keys(group.header)
+        self.wd.find_element(By.NAME, "group_footer").click()
+        self.wd.find_element(By.NAME, "group_footer").send_keys(group.footer)
+        # submit group creation
+        self.wd.find_element(By.NAME, "submit").click()
+
+    def open_groups_page(self):
+        self.wd.find_element(By.LINK_TEXT, "groups").click()
+
+    def login(self, username, password):
+        # login
+        self.wd.find_element(By.NAME, "user").send_keys(username)
+        self.wd.find_element(By.NAME, "pass").click()
+        self.wd.find_element(By.NAME, "pass").send_keys(password)
+        self.wd.find_element(By.CSS_SELECTOR, "input:nth-child(7)").click()
+
+    def open_home_page(self):
+        # open home page
+        self.wd.get("http://localhost/addressbook/")
